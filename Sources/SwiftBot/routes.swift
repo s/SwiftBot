fda7f83ed7a4f6994e431daf673f5434c7e56b18
Andrew Tokarev <git@atkit.com>
2017-05-30 21:56:18 +0200

    Simplified Routes, Removed HUB subscribe logic from Routes

--- Sources/SwiftBot/routes.swift
+++ Sources/SwiftBot/routes.swift
@@ -10,47 +10,42 @@ import Foundation
 import PerfectHTTP
 import PerfectHTTPServer
 
-
-internal func routes() -> Routes {
-    // Set routes
-    var routes = Routes(baseUri: "/")
-    routes.add(method: .get, uri: "/", handler: { request, response in
+internal func createHtmlRoute() -> Route {
+    return Route(method: .get, uri: "/", handler: { request, response in
         // Respond with a simple message.
         response.setHeader(.contentType, value: "text/html")
         response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
         // Ensure that response.completed() is called when your processing is done.
         response.completed()
     })
-    
-    let WebHookToken = "mega-secret-token-1"
+}
+
+internal func createWebhookRoutes() -> Routes {
     var webhook = Routes(baseUri: "/webhook")
     let webhookHandler: RequestHandler = { (request, response) in
-        Logger.info("query: \(request.queryParams)\n")
-        Logger.info("data: \(request.postParams)\n")
         
-        let hubMode = request.param(name: "hub.mode")
-        let hubToken = request.param(name: "hub.verify_token")
-        let hubChallenge = request.param(name: "hub.challenge")
-        
-        if let mode = hubMode, mode == "subscribe" {
-            
-            if let token = hubToken, let challenge = hubChallenge, token == WebHookToken {
-                Logger.info("Validating webhook\n");
-                
-                response.appendBody(string: challenge);
-                response.completed(status: .ok)
-            }
-            else {
-                response.completed(status: .forbidden);
-            }
-        }
-        else {
-            response.completed(status: .ok)
-        }
+        logRequest(request);
+
+        response.completed(status: .ok)
     }
     // Test: curl  -X GET "http://localhost:8080//webhook?hub.mode=subscribe&hub.challenge=27493587&hub.verify_token=mega-secret-token-1"
     webhook.add(method: .get, uri: "", handler: webhookHandler)
     webhook.add(method: .post, uri: "", handler: webhookHandler)
-    routes.add(webhook)
+    
+    return webhook;
+}
+
+private func logRequest(_ request: HTTPRequest) {
+    HerokuLogger.info("query: \(request.queryParams)\n")
+    HerokuLogger.info("data: \(request.postParams)\n")
+}
+
+internal func routes() -> Routes {
+    // Set routes
+    var routes = Routes(baseUri: "/")
+    
+    routes.add(createHtmlRoute())
+    routes.add(createWebhookRoutes())
+    
     return routes;
 }
