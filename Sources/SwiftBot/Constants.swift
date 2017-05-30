e7b9e9f22b993cad889fdfa10b904081a6a1f746
Andrew Tokarev <git@atkit.com>
2017-05-30 21:56:34 +0200

    Added Constants

---
+++ Sources/SwiftBot/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  SwiftBot
+//
+//  Created by Andrew on 30/05/2017.
+//
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct FacebookAPI {
+        
+        static let WebHookToken = "mega-secret-token-1"
+    }
+
+}
