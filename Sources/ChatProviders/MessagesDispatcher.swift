1e2a6f2a9f60092855f743ce79017ee85ebb12f5
Andrew Tokarev <git@atkit.com>
2017-06-14 22:07:17 +0200

    Extracted Provider protocol from MessageDispatcher as separate file

--- Sources/ChatProviders/MessagesDispatcher.swift
+++ Sources/ChatProviders/MessagesDispatcher.swift
@@ -6,30 +6,18 @@
 import Foundation
 
 public class MessagesDispatcher {
-    let services: [Provider]
+    let serviceProviders: [Provider]
     public let updatesHandler: Signal<Activity> = Signal()
     
     public init(services: [Provider]) {
-        self.services = services
-        self.register(services: self.services)
+        self.serviceProviders = services
+        self.register(services: self.serviceProviders)
     }
     
     public func send(activity: Activity) {
         debugPrint("Send message with service")
-        services.forEach{ $0.send(activity: activity) }
+        serviceProviders.forEach{ $0.send(activity: activity) }
     }
-}
-
-public protocol Provider: class {
-    weak var delegate: ProviderDelegate? { get set }
-    func parse(data: Data) throws
-    func parse(json: JSON) throws
-    
-    func send(activity: Activity)
-}
-
-public protocol ProviderDelegate: class {
-    func receive(message: Activity)
 }
 
 extension MessagesDispatcher: ProviderDelegate {
---
+++ Sources/ChatProviders/Provider.swift
@@ -0,0 +1,21 @@
+//
+//  Provider.swift
+//  SwiftBot
+//
+//  Created by Andrew on 14/06/2017.
+//
+//
+
+import Foundation
+
+public protocol Provider: class {
+    weak var delegate: ProviderDelegate? { get set }
+    func parse(data: Data) throws
+    func parse(json: JSON) throws
+    
+    func send(activity: Activity)
+}
+
+public protocol ProviderDelegate: class {
+    func receive(message: Activity)
+}
