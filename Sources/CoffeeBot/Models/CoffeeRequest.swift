bbb022120204b57af115c240b40ede862c4fcaa8
Said Ozcan <said.ozcan@booking.com>
2017-06-28 03:07:59 +0200

    add description to CoffeeRequest model

--- Sources/CoffeeBot/Models/CoffeeRequest.swift
+++ Sources/CoffeeBot/Models/CoffeeRequest.swift
@@ -9,4 +9,11 @@ import BotsKit
 internal struct CoffeeRequest {
     let person : Account
     let coffees : [Coffee]
+    
+    var description : String {
+        let coffeeDescriptions = coffees.map { (coffee) -> String in
+            return coffee.description
+        }
+        return "\(person.name) would like to have: \n \(coffeeDescriptions.joined(separator:" "))"
+    }
 }
