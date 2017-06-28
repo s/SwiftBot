20f5f5e3802dbd0c2e9c2defe9f29dac37282b34
Said Ozcan <said.ozcan@booking.com>
2017-06-28 03:15:27 +0200

    implement initial flow for processing a message

--- Sources/CoffeeBot/CoffeeManager/CoffeeManager.swift
+++ Sources/CoffeeBot/CoffeeManager/CoffeeManager.swift
@@ -5,6 +5,8 @@
 
 import Foundation
 import BotsKit
+import LoggerAPI
+
 internal protocol CoffeeManagerProtocol {
     func process(activity:Activity)
 }
@@ -12,7 +14,7 @@ internal protocol CoffeeManagerProtocol {
 class CoffeeManager {
     
     //MARK: Properties
-    fileprivate let session : Session
+    fileprivate var session : Session? = nil
     fileprivate let textProcessingService : TextProcessingService
     fileprivate let appointingService : AppointingService
     fileprivate let sessionService : SessionService
@@ -26,16 +28,27 @@ class CoffeeManager {
     }
     
     //MARK: Private
-    fileprivate func refreshSession() {
-        //read the current message session from db or disk
+    fileprivate func hasEveryoneReplied(in conversation:Conversation, with currentSession:Session) -> Bool {
+        return currentSession.membersReplied.count == conversation.members.count
     }
     
-    fileprivate func hasEveryoneReplied(for activity:Activity) -> Bool {
-        return self.session.membersReplied.count == activity.conversation.members.count
+    fileprivate func isTimeoutPeriodFinished() -> Bool {
+        //TODO: Set a timeout for messages
+        //E.g. all coffee requests should be made within 2 minutes.
+        return false
     }
     
-    fileprivate func acknowledgeInputs(with activity:Activity) {
-        let updatedActivity = activity.replay(text: self.session.description)
+    fileprivate func acknowledgeInputs(with activity:Activity, for session:Session) {
+        self.post(update: session.description, on: activity)
+    }
+    
+    fileprivate func postProcessingResult(chosenPerson:Account, session:Session, activity:Activity) {
+        let update = "\(chosenPerson.name) was selected to retrieve \(session.description). Congrats! ☕️"
+        self.post(update: update, on: activity)
+    }
+    
+    fileprivate func post(update text:String, on activity:Activity) {
+        let updatedActivity = activity.replay(text: text)
         self.botDelegate?.send(activity: updatedActivity)
     }
 }
@@ -43,13 +56,39 @@ class CoffeeManager {
 extension CoffeeManager : CoffeeManagerProtocol {
     
     func process(activity: Activity) {
-        self.refreshSession()
         
-        if hasEveryoneReplied(for: activity) {
-            self.acknowledgeInputs(with: activity)
+        //Restoring current context
+        self.session = self.sessionService.restoreSession()
+        guard var session = self.session else { return }
+        
+        //Processing the current message
+        let processingResult = self.textProcessingService.process(activity.text)
+        
+        //Saving the coffee to session
+        switch processingResult {
+        case .success(let coffees):
+            let coffeesRequested = CoffeeRequest(person: activity.from, coffees: coffees)
+            session.coffeesRequested.append(coffeesRequested)
+            self.sessionService.save(session: session)
+            
+        case .error(let error):
+            Log.error("An error occured during processing message:\(error.localizedDescription)")
+            return
+        }
+        
+        //Dispatching further events
+        if
+            hasEveryoneReplied(in: activity.conversation, with: session) ||
+            isTimeoutPeriodFinished()
+        {
+            //Printing acknowledgement of current requests
+            self.acknowledgeInputs(with: activity, for: session)
+            
+            self.post(update: "Finding the lucky one...", on: activity)
+            
             //proceed to next steps
-        } else {
-            //process message and save to session
+            let luckyPerson = self.appointingService.appointJobToAPerson(with: session)
+            self.postProcessingResult(chosenPerson: luckyPerson, session: session, activity: activity)
         }
     }
 }
